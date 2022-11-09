f01ba26137b7270bf9b5816c92d93753dfaa5efe
Evgeniy Klimovch <kliasanik@gmail.com>
2022-11-10 01:11:46 +0300

    add 2 buttons

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,22 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <button>MyYouTubeChanel-1</button>
-    </div>
-  );
+    const myFirstSubscriber = () => {
+        console.log("Hello Im Kevin"!)
+    }
+    const mySecondSubscriber = () => {
+        console.log("Hello Im Ivan"!)
+    }
+    return (
+        <div className="App">
+            {/*<button onClick={(event) => {*/}
+            {/*    console.log("Hello"!)*/}
+            {/*}}>MyYouTubeChanel-1*/}
+            {/*</button>*/}
+            <button onClick={myFirstSubscriber}> MyYouTubeChanel-1</button>
+            <button onClick={mySecondSubscriber}> MyYouTubeChanel-2</button>
+        </div>
+    );
 }
 
 export default App;
