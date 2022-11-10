ef0d4a1a975723d2edf038c421949ebba8fd2572
Evgeniy Klimovch <kliasanik@gmail.com>
2022-11-10 12:06:09 +0300

    add 2 buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, {MouseEvent} from 'react';
 import './App.css';
 
 
 function App() {
-    const myFirstSubscriber = (event:React.MouseEvent<HTMLButtonElement>) => {
-        console.log("Hello Im Kevin"!)
-    }
-    const mySecondSubscriber = (event:React.MouseEvent<HTMLButtonElement>) => {
-        console.log("Hello Im Ivan"!)
+    // const myFirstSubscriber = (event:MouseEvent<HTMLButtonElement>) => {
+    //     console.log("Hello Im Kevin"!)
+    // }
+    // const mySecondSubscriber = (event:MouseEvent<HTMLButtonElement>) => {
+    //     console.log("Hello Im Ivan"!)
+    // }
+    const onClickHandler = (name:string) => {
+        console.log(name)
     }
     return (
         <div className="App">
@@ -15,8 +18,8 @@ function App() {
             {/*    console.log("Hello"!)*/}
             {/*}}>MyYouTubeChanel-1*/}
             {/*</button>*/}
-            <button onClick={myFirstSubscriber}> MyYouTubeChanel-1</button>
-            <button onClick={mySecondSubscriber}> MyYouTubeChanel-2</button>
+            <button onClick={(event:MouseEvent<HTMLButtonElement>)=>onClickHandler("kevin")}> MyYouTubeChanel-1</button>
+            <button onClick={(event:MouseEvent<HTMLButtonElement>)=>onClickHandler("Ivan")}> MyYouTubeChanel-2</button>
         </div>
     );
 }
