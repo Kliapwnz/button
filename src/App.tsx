ac911b9224e2f9f83aad93ba1fdd4259a35097de
Evgeniy Klimovch <kliasanik@gmail.com>
2022-11-10 12:17:35 +0300

    add foo2

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,8 @@ function App() {
         console.log(100200)
 
     }
-    const foo2 = () => {
+    const foo2 = (name: string) => {
+        console.log(name)
 
     }
 
@@ -29,9 +30,10 @@ function App() {
             {/*<button onClick={(event:MouseEvent<HTMLButtonElement>)=>onClickHandler("kevin")}> MyYouTubeChanel-1</button>*/}
             {/*<button onClick={(event:MouseEvent<HTMLButtonElement>)=>onClickHandler("Ivan")}> MyYouTubeChanel-2</button>*/}
             <button onClick={foo1}>1</button>
-            <button>2</button>
+            <button onClick={(event:MouseEvent<HTMLButtonElement>)=>foo2("100200")}>2</button>
 
         </div>
     );
 }
+
 export default App;
