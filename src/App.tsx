dfc02af3a51a4f26d8e16c7814b6b5f29dd6d409
Evgeniy Klimovch <kliasanik@gmail.com>
2022-11-10 12:25:00 +0300

    delete all

--- src/App.tsx
+++ src/App.tsx
@@ -3,27 +3,13 @@ import './App.css';
 
 
 function App() {
-    // const myFirstSubscriber = (event:MouseEvent<HTMLButtonElement>) => {
-    //     console.log("Hello Im Kevin"!)
-    // }
-    // const mySecondSubscriber = (event:MouseEvent<HTMLButtonElement>) => {
-    //     console.log("Hello Im Ivan"!)
-    // }
-    const onClickHandler = (name:string) => {
-        console.log(name)
-    }
+
 
 
 
 
     return (
         <div className="App">
-            {/*<button onClick={(event) => {*/}
-            {/*    console.log("Hello"!)*/}
-            {/*}}>MyYouTubeChanel-1*/}
-            {/*</button>*/}
-            <button onClick={(event:MouseEvent<HTMLButtonElement>)=>onClickHandler("kevin")}> MyYouTubeChanel-1</button>
-            <button onClick={(event:MouseEvent<HTMLButtonElement>)=>onClickHandler("Ivan")}> MyYouTubeChanel-2</button>
 
         </div>
     );
