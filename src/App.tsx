5d605c41728d1bbda7c2d2e0d135b0fe7508544f
Evgeniy Klimovch <kliasanik@gmail.com>
2022-11-10 12:26:10 +0300

    create 2 buttons

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,10 @@ import './App.css';
 function App() {
 
 
-
-
-
     return (
         <div className="App">
-
+            <button>MyYouTubeChanel-1</button>
+            <button>MyYouTubeChanel-2</button>
         </div>
     );
 }
