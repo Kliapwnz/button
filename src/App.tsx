c6ff36b387010ee75971df0d796ebc9676a86e0b
Evgeniy Klimovch <kliasanik@gmail.com>
2022-11-10 12:14:50 +0300

    add console.log 100200

--- src/App.tsx
+++ src/App.tsx
@@ -9,19 +9,29 @@ function App() {
     // const mySecondSubscriber = (event:MouseEvent<HTMLButtonElement>) => {
     //     console.log("Hello Im Ivan"!)
     // }
-    const onClickHandler = (name:string) => {
-        console.log(name)
+    // const onClickHandler = (name:string) => {
+    //     console.log(name)
+    const foo1 = (event: MouseEvent<HTMLButtonElement>) => {
+        console.log(100200)
+
     }
+    const foo2 = () => {
+
+    }
+
+
     return (
         <div className="App">
             {/*<button onClick={(event) => {*/}
             {/*    console.log("Hello"!)*/}
             {/*}}>MyYouTubeChanel-1*/}
             {/*</button>*/}
-            <button onClick={(event:MouseEvent<HTMLButtonElement>)=>onClickHandler("kevin")}> MyYouTubeChanel-1</button>
-            <button onClick={(event:MouseEvent<HTMLButtonElement>)=>onClickHandler("Ivan")}> MyYouTubeChanel-2</button>
+            {/*<button onClick={(event:MouseEvent<HTMLButtonElement>)=>onClickHandler("kevin")}> MyYouTubeChanel-1</button>*/}
+            {/*<button onClick={(event:MouseEvent<HTMLButtonElement>)=>onClickHandler("Ivan")}> MyYouTubeChanel-2</button>*/}
+            <button onClick={foo1}>1</button>
+            <button>2</button>
+
         </div>
     );
 }
-
 export default App;
