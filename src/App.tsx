f4bd20343c83c349a412d8b194de5e3689b5f4bb
Evgeniy Klimovch <kliasanik@gmail.com>
2022-11-11 13:28:52 +0300

    add StupidButton

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,21 @@ import {Button} from "./components/Button";
 
 
 function App() {
-    const Button1Foo = (subscriber:string,age:number) => {
-        console.log(subscriber,age)
+    const Button1Foo = (subscriber: string, age: number) => {
+        console.log(subscriber, age)
     }
-    const Button2Foo = (subscriber:string) => {
+    const Button2Foo = (subscriber: string) => {
+        console.log(subscriber)
+    }
+    const Button3Foo = (subscriber: string) => {
         console.log(subscriber)
     }
 
-
     return (
         <div className="App">
-            <Button name={"MyYouTubeChanel-1"} callBack={()=>Button1Foo("Im Kevin",21)} />
-            <Button name={"MyYouTubeChanel-2"} callBack={()=>Button2Foo("Im Ivan")}/>
-
+            <Button name={"MyYouTubeChanel-1"} callBack={() => Button1Foo("Im Kevin", 21)}/>
+            <Button name={"MyYouTubeChanel-2"} callBack={() => Button2Foo("Im Ivan")}/>
+            <Button name={"Hello"} callBack={() => Button3Foo("Im Stupid Button")}/>
         </div>
     );
 }
