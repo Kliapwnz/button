0d513fbc293aa1c301397463f8abb3e2b7c950ed
Evgeniy Klimovch <kliasanik@gmail.com>
2022-11-24 15:30:57 +0300

    value " "

--- src/components/FullInput.tsx
+++ src/components/FullInput.tsx
@@ -4,17 +4,17 @@ type FullInputPropsType={
     addMessage:(title:string)=> void
 }
 export const FullInput = (props:FullInputPropsType) => {
-
     let [title, setTitle] = useState("")
     const onChangeInputHandler = (event: ChangeEvent<HTMLInputElement>) => {
         setTitle(event.currentTarget.value)
     }
 const onClickButtonHandler = () => {
         props.addMessage(title)
+    setTitle("")
 }
     return (
         <div>
-            <input onChange={onChangeInputHandler}/>
+            <input onChange={onChangeInputHandler} value={title}/>
             <button onClick={onClickButtonHandler}>+</button>
         </div>
     );
