8e1fb8250a669a3e5839e77c07c24ef6a4c35211
Evgeniy Klimovch <kliasanik@gmail.com>
2022-11-24 14:18:02 +0300

    create onChangeInputHandler

--- src/components/FullInput.tsx
+++ src/components/FullInput.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, {ChangeEvent, useState} from 'react';
 
 export const FullInput = () => {
+
+    let [title, setTitle] = useState("")
+    const onChangeInputHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        console.log(event.currentTarget.value)
+    }
+
     return (
         <div>
-            <input/>
-            <button>+</button>
+            <input onChange={onChangeInputHandler}/>
+            <button onClick={() => {
+            }}>+
+            </button>
         </div>
     );
 };
